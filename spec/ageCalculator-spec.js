d676a97f3a39f1d646ea02cbc3e1885fb44cf885
Qianqian Hu <qianhu@pindous.com>
2018-03-18 20:52:43 -0700

    hide the unit test of prototype

--- spec/ageCalculator-spec.js
+++ spec/ageCalculator-spec.js
@@ -11,9 +11,11 @@ describe('AgeCalculator',function(){
     expect(ageCalculator.dayOfBirth).toEqual(5);
     expect(ageCalculator.ageInYears).toEqual(28);
   });
-  // it('should correctly display users age by input the birthday in ageCalculator', function(){
-  //   var ageInYears = new AgeCalculator("1990-04-05", "89","1995-12-17")
-  //   expect(ageCalculator.ageInYears).toEqual(28);
-  // });
-
 });
+
+// describe('AgeCalculator',function(){
+//   it('should correctly check if user input real birthday', function(){
+//     var ageOnEarth = new AgeCalculator("2111-04-05", "89","1995-12-17")
+//     expect(ageOnEarth.earthAgeChecker).toEqual('Please enter your real birthday.');
+//   });
+// });
